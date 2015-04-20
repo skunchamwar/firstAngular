1b8e7028b42c52439b25d9d9da8f045f86a03ed6
Alejandro Saucedo <axsauze@gmail.com>
2015-04-20 15:28:14 +0100

    fix

--- Scripts/myModule.js
+++ Scripts/myModule.js
@@ -1,26 +1,32 @@
-﻿var sampleModule = angular.module('sampleModule', []);
-
-sampleModule.config(['$routeProvider', function ($routeProvider) {
+﻿angular
+    .module('sampleModule', [
+    'ngRoute',
+    ])
+    .config(function ($routeProvider) {
     $routeProvider
         .when('/view1',{
             templateUrl: 'view1.html',
-            controller: 'SimpleController',
+            // controller: 'SimpleController',
         }).when('/view2',{
             templateUrl: 'view2.html',
-            controller: 'SimpleController'
+            // controller: 'SimpleController'
         }).otherwise({ redirectTo: '/view1' });
-}]);
-sampleModule.controller('SimpleController', function SimpleController($scope) {
-    $scope.friends = [
-        { name: 'Sandeep', city: 'London' },
-        { name: 'Neha', city: 'Bristol' },
-        { name: 'Amey', city: 'Pune' }
-    ];
-
-    $scope.addFriend = function () {
-        $scope.friends.push({
-            name: $scope.newFriend.name,
-            city: $scope.newFriend.city
-        });
-    };
 });
+
+// angular.module('sampleModule')
+// .controller('SimpleController',[
+//     '$scope',
+//     function SimpleController($scope) {
+//         $scope.friends = [
+//             { name: 'Sandeep', city: 'London' },
+//             { name: 'Neha', city: 'Bristol' },
+//             { name: 'Amey', city: 'Pune' }
+//         ];
+
+//         $scope.addFriend = function () {
+//             $scope.friends.push({
+//                 name: $scope.newFriend.name,
+//                 city: $scope.newFriend.city
+//             });
+//         };
+//     });
